5aebcde820e390435eff0c9ddb486bdd7614a54d
Eleksinho <f.alvarezm@duocuc.cl>
2022-05-04 01:52:59 -0400

    efe

--- proyecto/js/login.js
+++ proyecto/js/login.js
@@ -1,7 +1,6 @@
 const nombre = document.getElementById("name") // CONSTANTE  ID NAME
 const email = document.getElementById("email") // CONSTANTE  ID MAIL
 const pass = document.getElementById("password") // CONSTANTE  ID PASSWORD
-const pass2 = document.getElementById("password2") // CONSTANTE  ID PASSWORD
 const form = document.getElementById("form") // CONSTANTE  ID FORM
 const parrafo = document.getElementById("warnings") // CONSTANTE  ID ADVERTENCIAS
 
@@ -11,26 +10,21 @@ form.addEventListener("submit", e=>{ //FUNCION PARA ESCUCHAR EL EVENTO SUBMIT, F
     let entrar = false
     let regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/ /* EXPRECIONES REGULARES, PARA QUE COINCIDA LOS CARACTERES DE LA CADENA @ - .COM */
     parrafo.innerHTML = ""
-    if(nombre.value.length <6){ //SI  NOMBRE ES  MAYOR A 6
-        warnings += ' - El nombre no es valido, debe ser mayor o igual a 6 caracteres. <br>' // CONCATENACION WARNING  Y MSJ
-        entrar = true
-    }
+    
+    
     if(!regexEmail.test(email.value)){ // VALIDAR LAS EXPRECIONES REGULARES DEL CORREO
-        warnings += ' - El email no es valido. <br>' // CONCATENACION WARNING  Y MSJ
+        warnings += ` - El email no es valido. <br>` // CONCATENACION WARNING  Y MSJ
         entrar = true
     }
     if(pass.value.length < 8){ //SI  CONTRASEÑA ES  MAYOR A 8
-        warnings += '- La contraseña no es valida, debe ser mayor o igual a 8 caracteres. <br>' // CONCATENACION WARNING  Y MSJ
-        entrar = true
-    }
-    if(pass2.value.length < 8){ //SI  CONTRASEÑA ES  MAYOR A 8
-        warnings += '- La contraseña no es valida, debe ser mayor o igual a 8 caracteres. <br>' // CONCATENACION WARNING  Y MSJ
+        warnings += ` - La contraseña no es valida, debe ser mayor o igual a 8 caracteres. <br>` // CONCATENACION WARNING  Y MSJ
         entrar = true
     }
 
     if(entrar){
         parrafo.innerHTML = warnings
     }else{
-        parrafo.innerHTML = "Enviado"
+        parrafo.innerHTML = "Correcto, entrando en 3 segundos..."
+        setTimeout( function() { window.location.href = "peliculas.html"; }, 3000 );  // CODIFO PARA DIRECCIONAR EN TIEMPO ESTA A 3000MILISEGUNDOS QUE SON 3 SG
     }
 })
